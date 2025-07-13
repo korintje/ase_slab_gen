04ae3f03633b11f3552e1cdb472b885b43bc1202
korintje <t.hosomi1410@gmail.com>
2025-07-13 23:52:47 +0900

    Modify usage example

--- usage_example.py
+++ usage_example.py
@@ -1,15 +1,14 @@
 from ase import Atoms
-from ase.io import write
 from ase.build import bulk
 import matplotlib.pyplot as plt
 from ase.visualize.plot import plot_atoms
 from general_surfaces import surfaces
 
-# Create a bulk (e.g. ZnO)
-my_bulk = bulk("OZn", crystalstructure="wurtzite", a=3.289, b=3.289, c=5.307, alpha=90.000, u=None)
+# Create a bulk
+zno_bulk = bulk("OZn", crystalstructure="wurtzite", a=3.289, b=3.289, c=5.307, alpha=90.000, u=None)
 
 # Set hkl indices
-h, k, l = 2, 0, 0
+h, k, l = 1, 0, 0
 
 # Define adsorbates
 hydroxyl = Atoms('OH', positions=[[0, 0, 0], [0, 0, 0.96]])
@@ -23,16 +22,12 @@ ads = [
     },            
 ]
 
-# Create all possible slabs
-slabs = surfaces(my_bulk, (h, k, l), 4, vacuum=10.0, adsorbates=ads, orthogonal=True)
+# Create all possible slabs (e.g. Two surfaces for ZnO(100))
+slabs = surfaces(zno_bulk, (h, k, l), 4, vacuum=5.0, adsorbates=ads)
 
 # Save slab images
+fig, axarr = plt.subplots(1, 2, figsize=(10, 5), dpi=300)
 for i, slab in enumerate(slabs):
     ext_slab = slab * (4, 4, 1)
-    fig, axarr = plt.subplots(1, 4, figsize=(20, 5), dpi=300)
-    plot_atoms(ext_slab, axarr[0], rotation=('0x,0y,0z'))
-    plot_atoms(ext_slab, axarr[1], rotation=('-90x,0y,0z'))
-    plot_atoms(ext_slab, axarr[2], rotation=('-90x,-90y,0z'))
-    plot_atoms(ext_slab, axarr[3], rotation=('30x,0y,0z'))
-    write(f"{i}.cif", slab)
-    fig.savefig(f"{i}.png")
+    plot_atoms(ext_slab, axarr[i], rotation=('-90x,-90y,0z'))
+fig.savefig(f"ZnO_100.png")
